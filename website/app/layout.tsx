1da710ed1f810555e9e74049ffc53ae494eabe3d
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-08-26 15:22:07 +0500

    feat: integrate Vercel Analytics for enhanced tracking and insights

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,11 +1,16 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export { metadata } from "./metadata";
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+
+        <Analytics />
+      </body>
     </html>
   );
 }
